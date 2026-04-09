64cd38a3889f03331b4af006cd041c8d02c72890
Adriano Germinal <adrianogerminal@gmail.com>
2026-04-09 11:38:15 +0200

    PHP OOP 5

--- index.php
+++ index.php
@@ -1,46 +1,33 @@
 <?php
 
+abstract class Category{
+    abstract public function getMyCategory();
+};
 
-
-//Completa la classe Fiat con le strutture mancanti e, utilizzando il livello di severita' che ritieni piu' opportuno, estendi i metodi per stampare a terminale la seguente frase: “La mia macchina e' Opel, con targa ND 123 OJ e nmero di Telaio 1234“.
-
-class Car {
-  private $num_telaio;
-
-  protected function setNumTelaio($num_telaio) 
-  {
-   return $this->num_telaio = $num_telaio;
-  }
-
-  protected function getNumTelaio() 
-    {
-        return $this->num_telaio;
+class Attualita extends Category{
+    public function getMyCategory(){
+        return "Sono nella categoria Attualità.";
     }
 };
 
-class Fiat extends Car {
-  protected $license;
-  protected $name;
-
-  public function __construct($license, $name,) {
-   $this->license = $license;  
-  $this->name = $name;
-  }
-
-  public function SetMyCarTelaio($num_telaio) {
-    return $this->setNumTelaio($num_telaio);
-  }
-
-    public function GetMyCarTelaio() {
-        return $this->getNumTelaio();
+class Sport extends Category{
+    public function getMyCategory(){
+        return "Sono nella categoria Sport.";
     }
+};
 
-    public function printCarInfo() {
-        echo "La mia macchina e' $this->name, con targa $this->license e numero di Telaio " . $this->GetMyCarTelaio() . ".";
+class Gossip extends Category{
+    public function getMyCategory(){
+        return "Sono nella categoria Gossip.";
     }
-}
+};
 
-$myCar = new Fiat("ND 123 OJ", "Opel");
-$myCar->SetMyCarTelaio("1234");
-$myCar->printCarInfo();
+class Storia extends Category{
+    public function getMyCategory(){
+        return "Sono nella categoria Storia.";
+    }
+};
+
+$storia = new Storia();
+echo $storia->getMyCategory();
 
